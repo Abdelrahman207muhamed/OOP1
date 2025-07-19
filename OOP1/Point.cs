f5b811e610587d139576408baa8d4b70520e47a0
user <user@DESKTOP-0FTCJBS>
2025-07-19 16:19:07 +0300

    Video 1

---
+++ OOP1/Point.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal struct Point
+    {
+        #region V1
+        //public int X = default; //4
+        //public int Y = default; //4
+
+        ////Constructor :Special Methods
+        ////              1.Named Like Struct 
+        ////              2.Has No Return Type  
+
+        ////CLR: Will Generate Parameterless Constructor
+        ////THIS Constructor Will Initialized The attributes With the Default value
+
+
+        ////Constructor
+        ////public Point() 
+        ////{
+
+        ////}
+
+        ////.NET 5.0 C# 9.0
+        ////.NET 6.0 C# 10.0
+        ////.NET 7.0 C# 11.0
+
+        ////.NET 8.0 C# 12.0
+
+        ////public Point()
+        ////{
+        ////    X = 12;
+        ////    Y = 12;
+        ////}
+
+
+
+        //public Point(int x, int y)
+        //{
+        //    X = x;
+        //    Y = y;
+        //}
+
+        //public void Printpoint()
+        //{
+        //    Console.WriteLine($"({X},{Y})"); 
+        //}
+
+
+        //public override string ToString()
+        //{
+        //    return $"({X},{Y})";
+        //}
+
+        #endregion
+
+    }
+}
--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -1,10 +1,55 @@
 ﻿namespace OOP1
 {
+    //1.Class
+    //2.Struct
+    //3.Interface
+    //4.Enum
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region V1
+            // //Struct : Value Type : Stack
+
+
+            // // Employee
+            // // ID, Name, Salary
+
+            // //  Point
+
+
+            // Point P01;
+            // // Declare For Object From Type 'Point'
+            // //P01:object
+            // //Allocate Uninitialized 8 Bytes at stack
+
+            // //Console.WriteLine(P01.X);
+            // //Console.WriteLine(P01.Y);
+
+            // //P01.X = 12;
+            // //P01.Y = 12;
+            // //Console.WriteLine(P01.X);
+            // //Console.WriteLine(P01.Y);
+
+            // P01 = new Point(1,2);
+            // // new:  Used for Select The Constructor
+
+
+            // //Console.WriteLine(P01.X);
+            // //Console.WriteLine(P01.Y);
+
+            // //Console.WriteLine(P01); //OOP1.Point
+            // //P01.Printpoint();
+
+            // //Console.WriteLine(P01.ToString());//OOP1.Point
+            //// Console.WriteLine(P01); //(1,2) 
+            #endregion
+
+
+
+
         }
     }
 }
