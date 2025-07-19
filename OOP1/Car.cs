122eff72f323c4923ec0b9ec3c8df8c508728dc0
user <user@DESKTOP-0FTCJBS>
2025-07-19 21:34:42 +0300

    Video 6

---
+++ OOP1/Car.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Car
+    {
+        // Id
+        #region Attributes
+        private double id;
+        private int speed;
+        private string model;
+        #endregion
+
+        #region Properties
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public double Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        #endregion
+
+        // CLR : Will Generate Parameterless Constructor
+        // This Constructor Do Nothing
+
+        // CTOR
+
+        //public Car()
+        //{
+
+        //} //  Do Nothing
+
+        //public Car()
+        //{
+        //	Id = default;
+        //	Model = default;
+        //	Speed = default;
+        //}
+
+        public Car(int id, int speed, string model) : this(id, speed)
+        {
+            //Id = id;
+            //Speed = speed;
+            Model = model;
+        }
+
+        public Car(int id, int speed) : this(id)
+        {
+            //Id = id;
+            Speed = speed;
+        }
+
+        public Car(int id)
+        {
+            Id = id;
+        }
+
+        public override string ToString()
+        {
+            return $"Id : {Id} :: Speed : {Speed} :: Model: {Model}";
+
+        
+        }
+
+        }
+    }
